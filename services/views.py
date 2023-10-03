acb682839d931f99ce437b51b086138d542e3334
Jeremías <martinbernardinoj@gmail.com>
2023-10-03 09:07:22 -0600

    init.py, requitements.txt

--- services/views.py
+++ services/views.py
@@ -3,5 +3,5 @@ from .models import Service  # Importa el modelo
 
 def services(request):
    # Lista de servicios para acceder al modelo
-   services = Service.objects.all()  # Utiliza "services.objects.all()"
+   services = Service.objects.all()  # Utiliza "services.objects.all()"         
    return render(request, "services/services.html", {'services': services})
--- webempresa/urls.py
+++ webempresa/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from core import views
 from django.conf import settings
 urlpatterns = [
     #Paths del core
